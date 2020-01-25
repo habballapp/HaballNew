989eda43df55dc2aaa5294f64de4d2c5f1446671
qadir <qadirabbasi1020@gmail.com>
2020-01-25 17:44:56 +0500

    proof_of_payment_form

--- app/src/main/java/com/example/haball/Payment/Proof_Of_Payment_Form.java
+++ app/src/main/java/com/example/haball/Payment/Proof_Of_Payment_Form.java
@@ -1,11 +1,15 @@
 package com.example.haball.Payment;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import com.example.haball.R;
 
@@ -25,6 +29,27 @@ public class Proof_Of_Payment_Form extends AppCompatActivity {
 
         actionBar.setCustomView(customView);
         actionBar.setDisplayShowCustomEnabled(true);
+        Button rl = (Button) findViewById(R.id.btn_upload);
+
+        rl.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+                final AlertDialog alertDialog = new AlertDialog.Builder(Proof_Of_Payment_Form.this).create();
+                LayoutInflater inflater = LayoutInflater.from(Proof_Of_Payment_Form.this);
+                View view_popup = inflater.inflate(R.layout.file_upload, null);
+                alertDialog.setView(view_popup);
+                ImageButton img_btn = view_popup.findViewById(R.id.image_button);
+                img_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+                    }
+                });
+                alertDialog.show();
+            }
+
+        });
+
     }
     }
 
