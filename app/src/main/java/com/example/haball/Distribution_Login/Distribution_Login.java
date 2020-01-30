68e7452d37b5e93c379f3b66655bd64148dc382d
qadir <qadirabbasi1020@gmail.com>
2020-01-30 13:58:24 +0500

    forgetpassword

--- app/src/main/java/com/example/haball/Distribution_Login/Distribution_Login.java
+++ app/src/main/java/com/example/haball/Distribution_Login/Distribution_Login.java
@@ -1,6 +1,7 @@
 package com.example.haball.Distribution_Login;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.haball.Distributor.DistributorDashboard;
+import com.example.haball.Payment.Payment_Screen2;
 import com.example.haball.R;
 import com.example.haball.Register_Activity.Register_Activity;
 import com.example.haball.Registration.Registration_Activity;
@@ -17,7 +20,8 @@ import com.example.haball.Support.Support_dashboard;
 
 public class Distribution_Login extends AppCompatActivity {
 
-    private Button btn_login,btn_signup,btn_support;
+    private Button btn_login,btn_signup,btn_support,btn_password,btn_reset;
+    private ImageButton btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,12 +30,16 @@ public class Distribution_Login extends AppCompatActivity {
         btn_login = findViewById(R.id.btn_login);
         btn_signup = findViewById(R.id.btn_signup);
         btn_support = findViewById(R.id.btn_support);
+        btn_password = findViewById(R.id.btn_password);
+        btn_back = (ImageButton)findViewById(R.id.btn_back);
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowHomeEnabled(false);
         actionBar.setDisplayShowTitleEnabled(false);
 
         LayoutInflater inflater = LayoutInflater.from(this);
         View customView = inflater.inflate(R.layout.action_bar_main, null);
+
 
         actionBar.setCustomView(customView);
         actionBar.setDisplayShowCustomEnabled(true);
@@ -58,5 +66,50 @@ public class Distribution_Login extends AppCompatActivity {
             }
         });
 
-    }
+        btn_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final AlertDialog alertDialog = new AlertDialog.Builder(Distribution_Login.this).create();
+                LayoutInflater inflater = LayoutInflater.from(Distribution_Login.this);
+                View view_popup = inflater.inflate(R.layout.forget_password, null);
+                alertDialog.setView(view_popup);
+                btn_reset = view_popup.findViewById(R.id.btn_reset);
+                ImageButton img_btn = view_popup.findViewById(R.id.image_button);
+                btn_reset.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+                        final AlertDialog alertDialog1 = new AlertDialog.Builder(Distribution_Login.this).create();
+                        LayoutInflater inflater = LayoutInflater.from(Distribution_Login.this);
+                        View view_popup = inflater.inflate(R.layout.email_sent, null);
+                        alertDialog1.setView(view_popup);
+                        ImageButton img_email = view_popup.findViewById(R.id.image_email);
+                        img_email.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                alertDialog1.dismiss();
+                            }
+                        });
+                        alertDialog1.show();
+
+                    }
+                });
+                img_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+
+                    }
+                });
+                alertDialog.show();
+
+            }
+
+        });
+
+
+            }
+
+
 }
