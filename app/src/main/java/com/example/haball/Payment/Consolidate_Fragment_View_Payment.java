46472a6a3208ac3a35073b7a24043bf9bb5f36a2
qadir <qadirabbasi1020@gmail.com>
2020-02-11 12:39:38 +0500

    consolidate_fragment_view_adapter ui

--- app/src/main/java/com/example/haball/Payment/Consolidate_Fragment_View_Payment.java
+++ app/src/main/java/com/example/haball/Payment/Consolidate_Fragment_View_Payment.java
@@ -4,6 +4,7 @@ package com.example.haball.Payment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,17 +17,24 @@ import com.example.haball.R;
  */
 public class Consolidate_Fragment_View_Payment extends Fragment {
 
-
-    public Consolidate_Fragment_View_Payment() {
-        // Required empty public constructor
-    }
-
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consolidate__fragment__view__payment, container, false);
-    }
+        View root = inflater.inflate(R.layout.fragment_consolidate__fragment__view__payment, container, false);
+//        recyclerView = root.findViewById(R.id.rv_consolidate);
+//
+//        recyclerView.setHasFixedSize(true);
+//
+//        // use a linear layout manager
+//        layoutManager = new LinearLayoutManager(getContext());
+//        recyclerView.setLayoutManager(layoutManager);
+//        mAdapter = new Consolidate_Fragment_Adapter(this,"Ghulam Rabani & Sons Traders & Distributors","1002312324251524","Shield","12/02/2020","PKR 600,000.00","600,000.00","Paid");
+//
+//        recyclerView.setAdapter(mAdapter);
 
+        return root;
+    }
 }
