22540594b201d3cfc22f7218c445c9bad75340fe
shabbir <haballapp@gmail.com>
2020-02-21 16:32:05 +0500

    shabbir

--- app/src/main/java/com/example/haball/Retailor/ui/Dashboard/DashBoardFragment.java
+++ app/src/main/java/com/example/haball/Retailor/ui/Dashboard/DashBoardFragment.java
@@ -16,11 +16,19 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -31,6 +39,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.haball.Distributor.DistributorPaymentsAdapter;
 import com.example.haball.Distributor.DistributorPaymentsModel;
+import com.example.haball.Distributor.ui.payments.MyJsonArrayRequest;
 import com.example.haball.Distributor.ui.payments.PaymentsViewModel;
 import com.example.haball.Payment.Payment_Amount;
 import com.example.haball.R;
@@ -55,11 +64,21 @@ public class DashBoardFragment extends Fragment {
     private PaymentsViewModel paymentsViewModel;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
-    private DrawerLayout drawer;
     private RecyclerView.LayoutManager layoutManager;
-    private String Token;
+    private String Token,DistributorId;;
     private String URL = "http://175.107.203.97:3020/api/prepaidrequests/Search";
     private List<RetailerPaymentModel> PaymentsList = new ArrayList<>();
+    //spiner1
+    private Spinner payment_retailer_spiner1;
+    private List<String> payment = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapterPayments_Ret;
+    private String Filter_selected, Filter_selected_value;
+    //spinner2
+    private Spinner payment_retailer_spiner2;
+    private List<String> payment_filters = new ArrayList<>();
+    private ArrayAdapter<String> arrayAdapter_PaymentFeltter;
+    private EditText edt_payment_ret;
+
 
     private Button create_payment;
 
@@ -68,12 +87,141 @@ public class DashBoardFragment extends Fragment {
         paymentsViewModel =
                 ViewModelProviders.of(this).get(PaymentsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard_retailor, container, false);
+        //init
         recyclerView = (RecyclerView) root.findViewById(R.id.rv_payment_request);
         create_payment = root.findViewById(R.id.create_payment);
+        payment_retailer_spiner1 = root.findViewById(R.id.spinner_dashboard_retailor);
+        edt_payment_ret = root.findViewById(R.id.edt_payment_ret);
+        payment_retailer_spiner2 = root.findViewById(R.id.payment_retailer_spiner);
+
+        payment_retailer_spiner2.setVisibility(View.GONE);
+        edt_payment_ret.setVisibility(View.GONE);
+        payment.add ("Select Criteria");
+        payment.add ("Company");
+        payment.add ("Payment Id");
+        payment.add ("Amount");
+        payment.add ("Status");
+
+        arrayAdapterPayments_Ret = new ArrayAdapter<>(root.getContext(),
+                android.R.layout.simple_spinner_dropdown_item, payment);
+        payment_retailer_spiner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0){
+                    ((TextView) adapterView.getChildAt(0)).setTextColor(getResources().getColor(android.R.color.darker_gray));
+                }
+                else{
+                    Filter_selected = payment.get(i);
+
+                    if(!Filter_selected.equals("Status"))
+                        payment_retailer_spiner2.setSelection(0);
+                    if(!edt_payment_ret.getText().equals(""))
+                        edt_payment_ret.setText("");
+
+                    if(Filter_selected.equals("Invoice No")) {
+                        Filter_selected = "ConsolidatedInvoiceNumber";
+                        payment_retailer_spiner2.setVisibility(View.GONE);
+                        edt_payment_ret.setVisibility(View.VISIBLE);
+                    } else if(Filter_selected.equals("Company")) {
+                        Filter_selected = "CompanyName";
+                        payment_retailer_spiner2.setVisibility(View.GONE);
+                        edt_payment_ret.setVisibility(View.VISIBLE);
+                    } else if(Filter_selected.equals("Created Date")) {
+                        payment_retailer_spiner2.setVisibility(View.GONE);
+                        edt_payment_ret.setVisibility(View.GONE);
+                        Toast.makeText(getContext(),"Created Date selected",Toast.LENGTH_LONG).show();
+                    } else if(Filter_selected.equals("Total Price")) {
+                        payment_retailer_spiner2.setVisibility(View.GONE);
+                        edt_payment_ret.setVisibility(View.GONE);
+                        Toast.makeText(getContext(),"Total Price selected",Toast.LENGTH_LONG).show();
+                    } else if(Filter_selected.equals("Paid Amount")) {
+                        payment_retailer_spiner2.setVisibility(View.GONE);
+                        edt_payment_ret.setVisibility(View.GONE);
+                        Toast.makeText(getContext(),"Paid Amount selected",Toast.LENGTH_LONG).show();
+                    } else if(Filter_selected.equals("Status")) {
+                        Filter_selected = "Status";
+                        payment_retailer_spiner2.setVisibility(View.VISIBLE);
+                        edt_payment_ret.setVisibility(View.GONE);
+                    } else if(Filter_selected.equals("Created By")) {
+                        Filter_selected = "CreatedBy";
+                        payment_retailer_spiner2.setVisibility(View.GONE);
+                        edt_payment_ret.setVisibility(View.VISIBLE);
+                    } else {
+                        payment_retailer_spiner2.setVisibility(View.GONE);
+                        edt_payment_ret.setVisibility(View.GONE);
+                    }
+//                    try {
+//                        fetchPaymentLedgerData(companies.get(Filter_selected));
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        arrayAdapterPayments_Ret.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        arrayAdapterPayments_Ret.notifyDataSetChanged();
+        payment_retailer_spiner1.setAdapter(arrayAdapterPayments_Ret);
+
+        //filter payment
+        payment_filters.add ("Status");
+        payment_filters.add ("Paid");
+        payment_filters.add ("Unpaid ");
+        arrayAdapter_PaymentFeltter = new ArrayAdapter<>(root.getContext(),
+                android.R.layout.simple_spinner_dropdown_item, payment_filters);
+
+        payment_retailer_spiner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == 0){
+                    ((TextView) adapterView.getChildAt(0)).setTextColor(getResources().getColor(android.R.color.darker_gray));
+                }
+                else{
+                    Filter_selected_value = String.valueOf(i-1);
+                    Log.i("Filter_selected_value",Filter_selected_value);
+                    try {
+                        fetchFilteredRetailerPayments();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        arrayAdapter_PaymentFeltter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        arrayAdapter_PaymentFeltter.notifyDataSetChanged();
+        payment_retailer_spiner2.setAdapter(arrayAdapter_PaymentFeltter);
+
+        edt_payment_ret.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+                Log.i("text1", "check");
+                Log.i("text", String.valueOf(s));
+                Filter_selected_value = String.valueOf(s);
+                try {
+                    fetchFilteredRetailerPayments();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
 
 
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
+
+        //recyclerview
         recyclerView.setHasFixedSize(true);
 
         // use a linear layout manager
@@ -139,6 +287,56 @@ public class DashBoardFragment extends Fragment {
                 params.put("Authorization", "bearer " +Token);
                 params.put("Content-Type", "application/json");
 
+                return params;
+            }
+        };
+        Volley.newRequestQueue(getContext()).add(sr);
+    }
+
+    private void fetchFilteredRetailerPayments() throws JSONException{
+        SharedPreferences sharedPreferences = getContext().getSharedPreferences("LoginToken",
+                Context.MODE_PRIVATE);
+        Token = sharedPreferences.getString("Login_Token", "");
+
+        SharedPreferences sharedPreferences1 = this.getActivity().getSharedPreferences("LoginToken",
+                Context.MODE_PRIVATE);
+        DistributorId = sharedPreferences1.getString("Distributor_Id", "");
+        Log.i("DistributorId ", DistributorId);
+        Log.i("Token", Token);
+
+        JSONObject map = new JSONObject();
+
+        map.put("TotalRecords", 10);
+        map.put("PageNumber", 0);
+        map.put(Filter_selected, Filter_selected_value);
+        Log.i("Mapsssss", String.valueOf(map));
+        JsonObjectRequest sr = new JsonObjectRequest(Request.Method.POST, URL,map, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject result) {
+                Log.i("retailerPayment", result.toString());
+
+                Gson gson = new Gson();
+                Type type = new TypeToken<List<RetailerPaymentModel>>(){}.getType();
+                try {
+                    PaymentsList = gson.fromJson(result.getJSONArray("PrePaidRequestData").toString(),type);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                mAdapter = new RetailerPaymentAdapter(getContext(),PaymentsList);
+                recyclerView.setAdapter(mAdapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "bearer " + Token);
+                params.put("Content-Type", "application/json; charset=UTF-8 ");
                 return params;
             }
         };
