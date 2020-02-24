96bfcf8c3a10d06fa98204287d756ae98346714e
RBK <rawahabinkhalid@gmail.com>
2020-02-25 00:55:59 +0500

    rbk

--- app/src/main/java/com/example/haball/Distributor/ui/payments/ViewPDFRequest.java
+++ app/src/main/java/com/example/haball/Distributor/ui/payments/ViewPDFRequest.java
@@ -3,11 +3,13 @@ package com.example.haball.Distributor.ui.payments;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.DocumentsContract;
@@ -21,11 +23,15 @@ import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.BuildConfig;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
@@ -74,33 +80,71 @@ public class ViewPDFRequest {
         map.put("TotalRecords", 10);
         map.put("PageNumber", 0.1);
 
-        StringRequest sr = new StringRequest(Request.Method.POST, URL_PDF_VIEW, new Response.Listener<String>() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+//        StringRequest sr = new StringRequest(Request.Method.POST, URL_PDF_VIEW, new Response.Listener<String>() {
+//            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+//            @Override
+//            public void onResponse(String result) {
+//                Log.i("PDF VIEW..", result);
+////                byte[] bytes = result.getBytes(Charset.forName("UTF-8"));
+//////                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//////                    try {
+//////                        byte[] decodedString = Base64.getDecoder().decode(new String(bytes).getBytes("UTF-8"));
+//////                        String string = new String(decodedString);
+//////                        Log.i("PDF BYTE DECODED..", string);
+//////
+//////                    } catch (UnsupportedEncodingException e) {
+//////                        e.printStackTrace();
+//////                    }
+//////
+//////                }
+//////                String string = new String(bytes);
+//////                Log.i("PDF BYTE ARRAY..", string);
+////                saveToFile(bytes, "file.pdf");
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                error.printStackTrace();
+//            }
+//        }) {
+//            @Override
+//            public Map<String, String> getHeaders() throws AuthFailureError {
+//                Map<String, String> params = new HashMap<String, String>();
+//                params.put("Authorization", "bearer " + Token);
+//                params.put("Content-Type", "application/json; charset=UTF-8");
+//                return params;
+//            }
+//        };
+//        Volley.newRequestQueue(context).add(sr);
+
+
+
+        InputStreamVolleyRequest request = new InputStreamVolleyRequest(Request.Method.POST, URL_PDF_VIEW, new Response.Listener<byte[]>() {
             @Override
-            public void onResponse(String result) {
-                Log.i("PDF VIEW..", result);
-//                byte[] bytes = result.getBytes(Charset.forName("UTF-8"));
-////                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-////                    try {
-////                        byte[] decodedString = Base64.getDecoder().decode(new String(bytes).getBytes("UTF-8"));
-////                        String string = new String(decodedString);
-////                        Log.i("PDF BYTE DECODED..", string);
-////
-////                    } catch (UnsupportedEncodingException e) {
-////                        e.printStackTrace();
-////                    }
-////
-////                }
-////                String string = new String(bytes);
-////                Log.i("PDF BYTE ARRAY..", string);
-//                saveToFile(bytes, "file.pdf");
+            public void onResponse(byte[] response) {
+                // TODO handle the response
+                try {
+                    if (response!=null) {
+                        String name = Environment.getExternalStorageDirectory() + "/myappname/" + "sample1.pdf";
+                        FileOutputStream fPdf = new FileOutputStream(name);
+
+                        fPdf.write(response);
+                        fPdf.flush();
+                        fPdf.close();
+                        Log.i("Download Complete", "Download complete.");
+                    }
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    Log.d("KEY_ERROR", "UNABLE TO DOWNLOAD FILE");
+                    e.printStackTrace();
+                }
             }
-        }, new Response.ErrorListener() {
+        } ,new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 error.printStackTrace();
             }
-        }) {
+        }, null)  {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
@@ -109,30 +153,7 @@ public class ViewPDFRequest {
                 return params;
             }
         };
-        Volley.newRequestQueue(context).add(sr);
-    }
-
-    public void saveToFile(byte[] byteArray, String pFileName){
-        File f = new File(Environment.getExternalStorageDirectory() + "/myappname");
-        if (!f.isDirectory()) {
-            f.mkdir();
-        }
-
-        String fileName = Environment.getExternalStorageDirectory() + "/myappname/" + pFileName;
-
-        try {
-
-            FileOutputStream fPdf = new FileOutputStream(fileName);
-
-            fPdf.write(byteArray);
-            fPdf.flush();
-            fPdf.close();
-            Toast.makeText(mContext, "File successfully saved", Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            Toast.makeText(mContext, "File create error", Toast.LENGTH_LONG).show();
-        } catch (IOException e) {
-            Toast.makeText(mContext, "File write error", Toast.LENGTH_LONG).show();
-        }
-
+        RequestQueue mRequestQueue = Volley.newRequestQueue(context, new HurlStack());
+        mRequestQueue.add(request);
     }
 }
