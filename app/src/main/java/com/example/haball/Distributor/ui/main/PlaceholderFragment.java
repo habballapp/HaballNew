de1c41dcd3d6916acfd7e10b73fe797c5ffc841d
Bilal Yaqoob <bilalyaqoob2207@gmail.com>
2020-03-13 18:00:57 +0500

    Merge branch 'master' of https://github.com/habballapp/HaballNew
    # Conflicts:
    #	app/src/main/java/com/example/haball/Distributor/ui/main/PlaceholderFragment.java

--- app/src/main/java/com/example/haball/Distributor/ui/main/PlaceholderFragment.java
+++ app/src/main/java/com/example/haball/Distributor/ui/main/PlaceholderFragment.java
@@ -574,7 +574,7 @@ public class PlaceholderFragment extends Fragment implements DatePickerDialog.On
         dateType = date_type;
         Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
 
-        DatePickerDialog dialog = new DatePickerDialog(getContext(), this,
+        DatePickerDialog dialog = new DatePickerDialog(getContext(),  R.style.DialogTheme, this,
                 calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
                 calendar.get(Calendar.DAY_OF_MONTH));
         dialog.show();
