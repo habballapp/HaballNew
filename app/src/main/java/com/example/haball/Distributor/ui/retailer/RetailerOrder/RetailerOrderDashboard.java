e7f82a805026e466dd6bac2d7aa85c70ab4fa296
Bilal Yaqoob <bilalyaqoob2207@gmail.com>
2020-03-14 17:24:03 +0500

    Merge branch 'master' of https://github.com/habballapp/HaballNew
    # Conflicts:
    #	app/src/main/java/com/example/haball/Distributor/ui/main/PlaceholderFragment.java

--- app/src/main/java/com/example/haball/Distributor/ui/retailer/RetailerOrder/RetailerOrderDashboard.java
+++ app/src/main/java/com/example/haball/Distributor/ui/retailer/RetailerOrder/RetailerOrderDashboard.java
@@ -1,19 +1,46 @@
 package com.example.haball.Distributor.ui.retailer.RetailerOrder;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.example.haball.Distributor.ui.orders.Adapter.CompanyFragmentAdapter;
+import com.example.haball.Distributor.ui.orders.Models.Company_Fragment_Model;
+import com.example.haball.Distributor.ui.payments.MyJsonArrayRequest;
+import com.example.haball.Distributor.ui.retailer.RetailerOrder.RetailerOrdersAdapter.RetailerOrdersAdapter;
+import com.example.haball.Distributor.ui.retailer.RetailerOrder.RetailerOrdersModel.RetailerOrdersModel;
 import com.example.haball.Distributor.ui.retailer.RetailerPlaceOrder.RetailerPlaceOrder;
 import com.example.haball.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RetailerOrderDashboard extends Fragment {
 
@@ -21,6 +48,9 @@ public class RetailerOrderDashboard extends Fragment {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private Button btn_place_order;
+    private String Token, DistributorId;
+    private String URL_FETCH_ORDERS = "http://175.107.203.97:4007/api/retailerorder/search";
+    private List<RetailerOrdersModel> OrdersList;
 
     private FragmentTransaction fragmentTransaction;
 
@@ -46,8 +76,66 @@ public class RetailerOrderDashboard extends Fragment {
 
             }
         });
+
+        try {
+            fetchRetailerOrdersData();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         return root;
 
+    }
+
+    private void fetchRetailerOrdersData() throws JSONException {
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("LoginToken",
+                Context.MODE_PRIVATE);
+        Token = sharedPreferences.getString("Login_Token", "");
+        Log.i("Token", Token);
+
+        SharedPreferences sharedPreferences1 = this.getActivity().getSharedPreferences("LoginToken",
+                Context.MODE_PRIVATE);
+        DistributorId = sharedPreferences1.getString("Distributor_Id", "");
+        Log.i("DistributorId ", DistributorId);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("DistributorId", DistributorId);
+        jsonObject.put("TotalRecords", 10);
+        jsonObject.put("PageNumber", 0);
+
+        MyJsonArrayRequest sr = new MyJsonArrayRequest(Request.Method.POST, URL_FETCH_ORDERS, jsonObject, new Response.Listener<JSONArray>() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onResponse(JSONArray result) {
+                Log.i("ORDERS DATA - ", result.toString());
+                Gson gson = new Gson();
+                Type type = new TypeToken<List<RetailerOrdersModel>>() {
+                }.getType();
+                OrdersList = gson.fromJson(result.toString(), type);
+                Log.i("OrdersList", String.valueOf(OrdersList));
+                mAdapter = new RetailerOrdersAdapter(getContext(), OrdersList);
+                recyclerView.setAdapter(mAdapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", "bearer " + Token);
+                params.put("Content-Type", "application/json; charset=UTF-8");
+                return params;
+            }
+        };
+        sr.setRetryPolicy(new DefaultRetryPolicy(
+                15000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        Volley.newRequestQueue(getContext()).add(sr);
     }
 
 }
