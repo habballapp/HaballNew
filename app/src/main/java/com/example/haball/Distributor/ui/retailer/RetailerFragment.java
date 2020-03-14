c0ad9930431ae23e94a8ce712fc3f4b8bf60ff2c
qadir <qadirabbasi1020@gmail.com>
2020-03-14 16:50:37 +0500

    Update Uis

--- app/src/main/java/com/example/haball/Distributor/ui/retailer/RetailerFragment.java
+++ app/src/main/java/com/example/haball/Distributor/ui/retailer/RetailerFragment.java
@@ -13,15 +13,14 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.haball.Distributor.ui.retailer.Retailor_Management.Adapter.Reatiler_Management_Dashboard_Adapter;
+import com.example.haball.Distributor.ui.retailer.Retailor_Management.Adapter.Retailer_Management_Dashboard_Adapter;
 import com.example.haball.R;
 
 public class RetailerFragment extends Fragment {
 
     private RetailerViewModel shareViewModel;
 
-    private RecyclerView recyclerView;
+    RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
 
@@ -29,18 +28,19 @@ public class RetailerFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         shareViewModel =
                 ViewModelProviders.of(this).get(RetailerViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_share, container, false);
+        View root = inflater.inflate(R.layout.fragment_retailer__dashboard, container, false);
 
-        recyclerView = (RecyclerView) root.findViewById(R.id.rv_reatailer_dashboard);
-
-        recyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
+        recyclerView = (RecyclerView) root.findViewById(R.id.rv_retailer_dashboard);
         layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
 
+        // use a linear layout manager
+
+
+
         // specify an adapter (see also next example)
-        mAdapter = new Reatiler_Management_Dashboard_Adapter("Ghulam Rabani & Sons Traders & Distributors","1002312324251524","334455","Paid");
+        mAdapter = new Retailer_Management_Dashboard_Adapter(getContext(), "Ghulam Rabani & Sons Traders & Distributors","1002312324251524","12/3/20","Paid");
         recyclerView.setAdapter(mAdapter);
 
         return root;
