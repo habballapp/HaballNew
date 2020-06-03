27989c42cd5f763210ed66e061eaf79a659dcbed
shabbir <haballapp@gmail.com>
2020-06-03 15:13:03 +0500

    abbasi

--- app/src/main/java/com/example/haball/Select_User/Register_Activity.java
+++ app/src/main/java/com/example/haball/Select_User/Register_Activity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.example.haball.Distribution_Login.Distribution_Login;
 import com.example.haball.R;
@@ -26,25 +27,25 @@ public class Register_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_);
-//        CardView card_retailer = findViewById(R.id.card_retailer);
-//        CardView card_distributor = findViewById(R.id.card_distributor);
+        RelativeLayout rl_distributor = findViewById(R.id.rl_distributor);
+        RelativeLayout rl_retailor = findViewById(R.id.rl_retailor);
 
-//        card_retailer.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v) {
-//
-//                Intent intent = new Intent(Register_Activity.this, RetailerLogin.class);
-//                startActivity(intent);
-//            }
-//
-//        });
-//        card_distributor.setOnClickListener(new View.OnClickListener(){
-//            public void onClick(View v) {
-//
-//                Intent intent = new Intent(Register_Activity.this, Distribution_Login.class);
-//                startActivity(intent);
-//            }
-//
-//        });
+        rl_distributor.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Register_Activity.this, Distribution_Login.class);
+                startActivity(intent);
+            }
+
+        });
+        rl_retailor.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Register_Activity.this, RetailerLogin.class);
+                startActivity(intent);
+            }
+
+        });
 
 
     }
